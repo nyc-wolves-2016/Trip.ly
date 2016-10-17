f92430596d6d17074fe4f01194b26f3bffd4e8a8
Jason Wong <jason.wong411@gmail.com>
2016-10-16 20:00:12 -0400

    fixed rerender bug with delete event functionality

--- app/assets/javascripts/components/Event.es6.jsx
+++ app/assets/javascripts/components/Event.es6.jsx
@@ -1,8 +1,21 @@
 class Event extends React.Component {
   constructor() {
     super();
+    this.state = {
+      events: []
+    }
     this.handleEditButtonClick = this.handleEditButtonClick.bind(this);
-    // this.handleEventEdit = this.handleEventEdit.bind(this);
+    // this.handleDelete = this.handleDelete.bind(this);
+    this.handleDeleteButtonClick = this.handleDeleteButtonClick.bind(this);
+
+  }
+
+  componentDidMount() {
+    // debugger;
+    this.setState({
+      events: this.props.events
+    })
+    // debugger;
   }
 
   handleEditButtonClick(event) {
@@ -17,14 +30,41 @@ class Event extends React.Component {
 
   }
 
+  handleDeleteButtonClick(event) {
+    event.preventDefault();
+    // debugger;
+    var url = $(event.target).attr('href');
+    var event = this.props.data
+    $.ajax({
+      url: url,
+      method: "delete",
+      data: event
+    })
+    .done(function(response){
+      // debugger;
+      // this.props.handleEventDelete(response);
+      this.props.onEventDelete(response);
+      // debugger;
+      // this.forceUpdate();
+    }.bind(this))
+  }
+
+  // handleDelete(response){
+  //   this.setState({
+  //     events: response
+  //   });
+  //   // debugger;
+  //   this.forceUpdate();
+  // }
+
   render() {
     let { name, location, details, contact_info, date, start_time, end_time, id } = this.props.data;
-    // debugger;
     // TRY MOVING THE FORM/BUTTON NEXT TO THE EVENT COMPONENT IN ITINERARY
     return(
       <div>
         <div>
           <a className="event-edit" href={"/trips/" + this.props.itinerary.itinerary.trip_id + "/itineraries/" + this.props.itinerary.itinerary.id + "/events/" + id } onClick={this.handleEditButtonClick}>Edit Event</a>
+          <a className="event-delete" href={"/trips/" + this.props.itinerary.itinerary.trip_id + "/itineraries/" + this.props.itinerary.itinerary.id + "/events/" + id } onClick={this.handleDeleteButtonClick}>Delete Event</a>
         </div>
 
           <li>
