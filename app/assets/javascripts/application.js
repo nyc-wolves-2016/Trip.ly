5eb2201ce11caee929af23351137579894927d87
Kimberly Suazo <email": null>
2016-10-18 22:10:23 -0400

    Add styling to user show page. Add images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 //= require_tree .
 
 
+
 function initAutocomplete() {
   var autocomplete = new google.maps.places.Autocomplete(
            (document.getElementById('trip_city')),
@@ -41,10 +42,10 @@ $(document).ready(function() {
       html += '<li class="heading">'+weather.alt.temp+'&deg;C</li></ul>';
 
       for(var i=0;i<6;i++) {
-        html += '<ul class="test"><li class="test">'+weather.forecast[i].day+'</li>';
+        html += '<ul class="test"><li class="bold">'+weather.forecast[i].day+'</li>';
         html += '<li>'+weather.forecast[i].high+'F</li>';
         html += '<li>'+weather.forecast[i].low+'F</li>';
-        html += '<li><img src="'+weather.forecast[i].thumbnail+'"></li></ul>';
+        html += '<li class="weather-icon"><img src="'+weather.forecast[i].thumbnail+'"></li></ul>';
 
       }
 
