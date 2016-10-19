cbb4d4873e13d3654d3acf9dc84dafa794c6333f
Jenn Su <jennyteasoup@gmail.com>
2016-10-19 12:59:30 -0400

    Fix bug for adding new resource list

--- app/assets/javascripts/components/Holder.es6.jsx
+++ app/assets/javascripts/components/Holder.es6.jsx
@@ -19,6 +19,7 @@ class Holder extends React.Component {
     this.handleAddResourcePreviewForm = this.handleAddResourcePreviewForm.bind(this);
     this.handleResetHolder = this.handleResetHolder.bind(this);
     this.handleNewPackingList = this.handleNewPackingList.bind(this);
+    this.handleNewResourceList = this.handleNewResourceList.bind(this);
   }
 
   componentDidMount() {
@@ -37,6 +38,12 @@ class Holder extends React.Component {
   handleNewPackingList(lists){
     this.setState({
       packing_lists: lists
+    });
+  }
+
+  handleNewResourceList(lists){
+    this.setState({
+      resource_lists: lists
     });
   }
 
@@ -94,7 +101,8 @@ class Holder extends React.Component {
           <div className="large-8 column large-centered">
             { this.state.itineraryPreview ? <ItineraryPreview onItineraryClick={this.handleNestedItinerary} trip={trip}/> : null }
             { this.state.packingPreview ? <PackingLists onNewPackingList={this.handleNewPackingList} onListClick={this.handleNested} trip={trip} packing_lists={this.state.packing_lists} onErrors={this.handleNestedErrors} errors={this.props.errors} onAddPackingPreviewForm={this.handleAddPackingPreviewForm} onResetHolder={this.handleResetHolder} /> : null }
-            { this.state.resourcePreview ? <ResourceListPreview onResourceListClick={this.handleNestedResource} trip={trip} resource_lists={this.state.resource_lists}  onErrors={this.handleNestedErrors} errors={this.props.errors} onAddResourcePreviewForm={this.handleAddResourcePreviewForm} onResetHolder={this.handleResetHolder} /> : null }
+            { this.state.resourcePreview ? <ResourceListPreview onResourceListClick={this.handleNestedResource} trip={trip} resource_lists={this.state.resource_lists}  onErrors={this.handleNestedErrors}
+            onNewResourceList={this.handleNewResourceList} errors={this.props.errors} onAddResourcePreviewForm={this.handleAddResourcePreviewForm} onResetHolder={this.handleResetHolder} /> : null }
         </div>
       </div>
     )
