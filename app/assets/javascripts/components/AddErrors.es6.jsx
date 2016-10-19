9f5297289265a6c139f7898909c9c5fc870381c6
Jenn Su <jennyteasoup@gmail.com>
2016-10-19 16:26:59 -0400

    style packing list items view

--- app/assets/javascripts/components/AddErrors.es6.jsx
+++ app/assets/javascripts/components/AddErrors.es6.jsx
@@ -2,13 +2,11 @@ class AddErrors extends React.Component {
   render() {
     let { errors } = this.props;
     return(
-      <div>
         <ul>
           {Object.keys(errors).map(function(key, i) {
             return <li key={i}>{key} {errors[key]}</li>;
           })}
         </ul>
-      </div>
     )
   }
 }
