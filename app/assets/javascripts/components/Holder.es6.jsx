29a7016aaf53feb69ee331888ab2d7a8bdcb0522
Angel Chen <rikugou@gmail.com>
2016-10-18 21:30:51 -0400

    Style event and add packing list form

--- app/assets/javascripts/components/Holder.es6.jsx
+++ app/assets/javascripts/components/Holder.es6.jsx
@@ -68,18 +68,12 @@ class Holder extends React.Component {
 
     return(
       <div className="row">
-        <div>
-          { this.state.itineraryPreview ? <ItineraryPreview onItineraryClick={this.handleNestedItinerary} trip={trip}/> : null }
+          <div className="large-8 column large-centered">
+            { this.state.itineraryPreview ? <ItineraryPreview onItineraryClick={this.handleNestedItinerary} trip={trip}/> : null }
+            { this.state.packingPreview ? <PackingLists onListClick={this.handleNested} trip={trip} packing_lists={packing_lists} onErrors={this.handleNestedErrors} errors={this.props.errors} onAddPackingPreviewForm={this.handleAddPackingPreviewForm} onResetHolder={this.handleResetHolder}/> : null }
+            { this.state.resourcePreview ? <ResourceListPreview onResourceListClick={this.handleNestedResource} trip={trip} resource_lists={resource_lists}  onErrors={this.handleNestedErrors} errors={this.props.errors} onAddResourcePreviewForm={this.handleAddResourcePreviewForm} onResetHolder={this.handleResetHolder}/> : null }
         </div>
-          { this.state.packingPreview ? <PackingLists onListClick={this.handleNested} trip={trip} packing_lists={packing_lists} onErrors={this.handleNestedErrors} errors={this.props.errors} onAddPackingPreviewForm={this.handleAddPackingPreviewForm} onResetHolder={this.handleResetHolder}/> : null }
-          { this.state.resourcePreview ? <ResourceListPreview onResourceListClick={this.handleNestedResource} trip={trip} resource_lists={resource_lists}  onErrors={this.handleNestedErrors} errors={this.props.errors} onAddResourcePreviewForm={this.handleAddResourcePreviewForm} onResetHolder={this.handleResetHolder}/> : null }
-        <div>
-
-
-        </div>
-
       </div>
-
     )
   }
 }
