8a026f3506c00a883ad3cdced17ca9c634504ce1
Kimberly Suazo <email": null>
2016-10-20 15:21:24 -0400

    Add link validation

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,4 +2,6 @@ class Resource < ApplicationRecord
   belongs_to :resource_list
 
   validates :name, presence: true
+  validates :link,  format: { with: /\A#{URI::regexp(['http', 'https'])}\z/,
+    message: "must begin with http:// or https://" }
 end
